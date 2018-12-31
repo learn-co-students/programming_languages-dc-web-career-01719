19e6dca3c02fd60ffe423f399fc7e71913786c32
Kyle Houghton <kylebenz@hotmail.com>
2018-12-31 05:19:51 +0000

    All tests passing.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,35 @@
-def reformat_languages(languages)
-  # your code here
+
+def output_style_a(data_structure, language)
+  return_array = []
+  data_structure.each do |key, value|
+    return_array << key if value.keys.any? {|key| key == language} 
+  end
+  return_array
+end
+
+def output_type_s(data_structure, language)
+  return_string = ""
+  data_structure.each do |key, value|
+    value.each do |lang, lang_type|
+      return_string = lang_type.values.join if lang == language
+    end
+  end
+  return_string
+end
+
+def build_languages_array(data_structure)
+  lang_array = []
+  data_structure.each do |key, value|
+    lang_array << value.keys
+  end
+  lang_array.flatten.uniq
+end
+
+def reformat_languages(data_structure)
+  return_hash = {}
+  build_languages_array(data_structure).each do |language|
+    return_hash[language] = 
+    {:type => output_type_s(data_structure, language), :style => output_style_a(data_structure, language)}
+  end
+  return_hash
 end
